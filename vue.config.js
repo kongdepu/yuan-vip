cbc2baa980ab39cda89e916ec6a8bbc20974f514
kongdepu <kongdepu@126.com>
2022-10-25 10:57:16 +0800

    push

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,6 @@ module.exports = {
   lintOnSave: process.env.NODE_ENV === 'development' ? false : true,
   productionSourceMap: false,
   devServer: {
-    disableHostCheck: true,
     port: port,
     open: true,
     overlay: {
